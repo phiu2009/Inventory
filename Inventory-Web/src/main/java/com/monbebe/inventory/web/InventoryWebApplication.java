bbbed0e817edb03aa043669c2b0e40643678f63b
PPP <phiu2009@gmail.com>
2016-01-19 16:13:33 +1100

    Initial project setup

---
+++ Inventory-Web/src/main/java/com/monbebe/inventory/web/InventoryWebApplication.java
@@ -0,0 +1,12 @@
+package com.monbebe.inventory.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryWebApplication.class, args);
+	}
+}
